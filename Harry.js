8c0a72059e46683d665d680fc60214acbca95dfa
leilalinoo <aleila0921@gmail.com>
2023-05-17 09:33:04 +0200

    valtoztatasok

---
+++ Harry.js
@@ -0,0 +1,12 @@
+class Harry{
+    #eletero
+    #varazsero
+    #kep
+    #x
+    #y
+    constructor(eletero, varazsero){
+        this.#kep = "Harry/seprusharry.png";
+        this.#eletero = 5
+        this.#varazsero = 2
+    }
+}
