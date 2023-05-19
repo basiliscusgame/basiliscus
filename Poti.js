18da69c280446108c4cb79f60a91a256fd4ecc73
Wendesday36 <bertazoeanna@gmail.com>
2023-05-19 12:34:54 +0200

    sxwx

--- Poti.js
+++ Poti.js
@@ -3,19 +3,21 @@ class Poti{
     #ero
     #x
     #y
-    
-    constructor(){
+    #DivElem
+    constructor(DivElem,kep){
+        this.#DivElem = $("article")
         this.#ero = ero
         this.#x= x
         this.#y = y
-
+        DivElem.append("<div></div>")
     }
 
     KEP() {
         for (let index = 0; index < PICTURES.length; index++) {
-            const kep = Math.floor(Math.random()*3);
+            const kep =  ICTURES[Math.floor(Math.random()*PICTURES.length)]}" alt = "#" >` 
             
         }
+
     }
 
 }
