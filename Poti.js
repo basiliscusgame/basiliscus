1bc69fa139bc8133694fd45117dda850b9e254d6
leilalinoo <aleila0921@gmail.com>
2023-05-19 13:26:23 +0200

    Poti ereje generalass

--- Poti.js
+++ Poti.js
@@ -6,13 +6,15 @@ class Poti{
     #DivElem
     constructor(DivElem,kep){
         this.#DivElem = $("article")
-        this.#ero = ero
+        this.#ero = this.eroGeneral();
         this.#x= x
         this.#y = y
         DivElem.append(`<img src ="${PICTURES[0]}" alt = "#" >`)
     }
 
-    
+    eroGeneral(){
+        this.#ero = Math.floor(Math.random()*5)+2
+    }
 
 }
 export default Poti
