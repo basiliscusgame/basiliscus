d949091e22f905f5e3ef9afbaea1c367e10ba195
Wendesday36 <bertazoeanna@gmail.com>
2023-05-19 12:07:58 +0200

    POTI CONSTRUKTOR

--- Poti.js
+++ Poti.js
@@ -1,4 +1,21 @@
+import { PICTURES } from "./poti_pictures.js"
 class Poti{
     #ero
-    #
+    #x
+    #y
+    
+    constructor(){
+        this.#ero = ero
+        this.#x= x
+        this.#y = y
+
+    }
+
+    KEP() {
+        for (let index = 0; index < PICTURES.length; index++) {
+            const kep = Math.floor(Math.random()*3);
+            
+        }
+    }
+
 }
---
+++ poti_pictures.js
@@ -0,0 +1,7 @@
+export const PICTURES= [
+    "Harry/potion1.png",
+    "Harry/potion2.png",
+    "Harry/potion3.png",
+    "Harry/labda.png",
+]
+    
