e521356f5ac7846a2065db55ddbb6a54c969272f
leilalinoo <aleila0921@gmail.com>
2023-05-19 13:42:18 +0200

    koordinatak torlese, style

--- Poti.js
+++ Poti.js
@@ -1,20 +1,18 @@
 import { PICTURES } from "./poti_pictures.js"
 class Poti{
     #ero
-    #x
-    #y
     #DivElem
-    constructor(DivElem,kep){
+    constructor(DivElem){
         this.#DivElem = $("article")
         this.#ero = this.eroGeneral();
-        this.#x= x
-        this.#y = y
         DivElem.append(`<img src ="${PICTURES[0]}" alt = "#" >`)
     }
 
     eroGeneral(){
         this.#ero = Math.floor(Math.random()*5)+2
     }
+
+
 
 }
 export default Poti
