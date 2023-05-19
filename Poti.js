3aee8a6e54cd0113d40ad41c214d2e9b9d07adda
Wendesday36 <bertazoeanna@gmail.com>
2023-05-19 13:28:21 +0200

    divelem

--- Poti.js
+++ Poti.js
@@ -1,25 +1,18 @@
-import { PICTURES } from "./poti_pictures.js"
-class Poti{
-    #ero
-    #x
-    #y
-    #DivElem
-    constructor(DivElem,kep){
-<<<<<<< HEAD
-       
-        this.#ero = ero
-=======
-        this.#DivElem = $("article")
-        this.#ero = this.eroGeneral();
->>>>>>> a0288b006d90cb8038bc218fa57acbc80fc0e2ac
-        this.#x= x
-        this.#y = y
-        DivElem.append(`<img src ="${PICTURES[0]}" alt = "#" >`)
-    }
+import { PICTURES } from "./poti_pictures.js";
+class Poti {
+  #ero;
+  #x;
+  #y;
+  #DivElem;
+  constructor(DivElem, kep) {
+    this.#ero = this.eroGeneral();
+    this.#x = x;
+    this.#y = y;
+    DivElem.append(`<img src ="${PICTURES[0]}" alt = "#" >`);
+  }
 
-    eroGeneral(){
-        this.#ero = Math.floor(Math.random()*5)+2
-    }
-
+  eroGeneral() {
+    this.#ero = Math.floor(Math.random() * 5) + 2;
+  }
 }
-export default Poti
+export default Poti;
