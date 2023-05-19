23d7d0e8666e383222c0bfdc924aa92d986e9369
leilalinoo <aleila0921@gmail.com>
2023-05-19 12:08:26 +0200

    osztalyok

---
+++ Voldi.js
@@ -0,0 +1,17 @@
+class Voldi{
+    #kep
+    #elet
+    constructor(elet){
+        this.#kep = "Harry/voldemort.png"
+
+    }
+
+    elethalal(ms) {
+        
+        
+
+    }
+
+}
+
+export default Voldi
